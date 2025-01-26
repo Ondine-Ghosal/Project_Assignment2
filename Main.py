7c8eca80f174c6d4a2fbbbc341c6dfa7e2962daa
Ondine-Ghosal <ondine.ghosal@gmail.com>
2025-01-26 08:21:05 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,2 @@
-print(*(n for n in range(2000, 3201) if n % 7 == 0 and n % 5 != 0), sep=",")
+print(*(n for n in range(2000, 3201) if n % 7 == 0 and n % 5 != 0), sep=",") 
+# this is seeing all the number in range 2000 to 3201 then checks if it is a muliple of 7 and not 5 then prints it out comma seperated
